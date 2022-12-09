79d1d40de411a5159dce68d6f74c27be1fd6be19
rufflabs <jetaylor@gmail.com>
2022-12-08 23:42:49 -0600

    Fixed web01 missing mysqli (php-mysqlnd) and html directory contents.

--- scripts/web01/apache-php.sh
+++ scripts/web01/apache-php.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DEBIAN_FRONTEND=noninteractive apt install -y apache2 php php-cli libapache2-mod-php
+DEBIAN_FRONTEND=noninteractive apt install -y apache2 php php-cli libapache2-mod-php php-mysqlnd
--- scripts/web01/bwapp.sh
+++ scripts/web01/bwapp.sh
@@ -4,6 +4,9 @@ echo "Copying website files..."
 mkdir -p /var/www/html/logs
 cp -r /vagrant/files/bWAPP/* /var/www/html/
 
+cp -f /vagrant/scripts/web01/settings.php /var/www/html/admin/
+rm -f /var/www/html/index.html
+
 # Set permissions according to install document
 echo "Modifying permissions..."
 chmod 777 /var/www/html/passwords /var/www/html/images /var/www/html/documents /var/www/html/logs
