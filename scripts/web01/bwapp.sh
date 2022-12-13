c75d10c0082ca574c8df5f89256668e96a460d4e
rufflabs <jetaylor@gmail.com>
2022-12-12 21:21:41 -0600

    bwapp install from web

--- scripts/web01/bwapp.sh
+++ scripts/web01/bwapp.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-
-echo "Copying website files..."
 mkdir -p /var/www/html/logs
-cp -r /vagrant/files/bWAPP/* /var/www/html/
 
+# Check if bWAPP is already available in /vagrant/files
+if [ ! -f "/vagrant/files/bWAPPv2.2.zip" ]
+then
+# Download bWAPP
+echo "Downloading bWAPP..."
+wget https://sourceforge.net/projects/bwapp/files/bWAPP/bWAPPv2.2/bWAPPv2.2.zip -O /vagrant/files/bWAPPv2.2.zip
+fi
+
+# Unzip bWAPP
+echo "Extracting bWAPP..."
+mkdir /tmp/bWAPP
+cd /tmp/bWAPP
+unzip /vagrant/files/bWAPPv2.2.zip
+cp -r /tmp/bWAPP/bWAPP/* /var/www/html/
 cp -f /vagrant/scripts/web01/settings.php /var/www/html/admin/
 rm -f /var/www/html/index.html
 
 # Set permissions according to install document
-echo "Modifying permissions..."
+echo "Setting permissions..."
 chmod 777 /var/www/html/passwords /var/www/html/images /var/www/html/documents /var/www/html/logs
