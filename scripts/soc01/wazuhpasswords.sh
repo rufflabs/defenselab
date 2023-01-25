c8a7780e6740c40634aea5424b901ab51cef7cca
rufflabs <jetaylor@gmail.com>
2023-01-24 23:20:24 -0600

    Wazuh passwords were being saved in root dir

--- scripts/soc01/wazuhpasswords.sh
+++ scripts/soc01/wazuhpasswords.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Extract the Wazuh passwords
-tar -O -xvf /home/vagrant/wazuh-install-files.tar wazuh-install-files/wazuh-passwords.txt > /vagrant/wazuh-passwords.txt
+tar -O -xvf /home/vagrant/wazuh-install-files.tar wazuh-install-files/wazuh-passwords.txt > /vagrant/files/wazuh-passwords.txt
